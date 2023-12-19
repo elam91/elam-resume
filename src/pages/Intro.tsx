0e5e2135db38ec0b0c6ec17d65ff3919849fe547
elam91 <elam91@gmail.com>
2023-12-19 13:37:40 +0200

    more intro changes

--- src/pages/Intro.tsx
+++ src/pages/Intro.tsx
@@ -5,7 +5,7 @@ import Signature from "../components/Signature";
 const Intro = () => {
   const { data, isFetching } = useGetLongDescription({ page: PageEnum.INTRO });
   return (
-    <div className="md:min-h-full lg:min-h-0 lg:max-h-none w-full relative bg-white  bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-40 border border-gray-500 min-h-full rounded-xl flex flex-col">
+    <div className="min-h-full max-h-none w-full relative bg-white  bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-40 border border-gray-500 rounded-xl flex flex-col justify-start">
       {!data ? (
         <div className="animate-pulse w-full h-full flex justify-center mt-10">
           <p className="text-5xl ">Loading...</p>
@@ -13,7 +13,7 @@ const Intro = () => {
       ) : (
         <div className="flex flex-col h-full">
           <div
-            className="p-5 min-h-max  prose text-gray-800 lg:prose-xl font-light prose-headings:font-extralight prose-li:marker:text-black  max-w-none"
+            className="p-5 min-h-max  prose lg:prose-sm prose-base xl:prose-base text-gray-800  font-light prose-headings:font-extralight prose-li:marker:text-black  max-w-none"
             dangerouslySetInnerHTML={{ __html: data?.body || "" }}
           ></div>
           <div className="w-full md:flex flex-row justify-end hidden  p-0 pb-4 pr-4 h-44 items-end">
