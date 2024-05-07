569fdefb5db7e0aa611e31a5d1f6b28f929c4729
elam91 <elam91@gmail.com>
2024-05-07 23:07:21 +0300

    add pdf to end of file

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -20,7 +20,7 @@ const Header = () => {
                 download={`Elam Buteil - Software Developer - ${format(
                   new Date(),
                   "MMMM do y"
-                )}`}
+                )}.pdf`}
                 className="text-white bg-gradient-to-bl from-myPurple-100 to-myPurple-800 hover:bg-myPurple-100 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 focus:outline-none "
               >
                 Download to PDF
