319ce6f31f49c21f2a113db58b7d075064b87a30
Elam Buteil <elam91@gmail.com>
2025-08-19 12:04:18 +0300

    fix skeletons

--- src/components/Projects/ProjectSkeleton.tsx
+++ src/components/Projects/ProjectSkeleton.tsx
@@ -3,7 +3,7 @@ import range from "lodash.range";
 
 const ProjectSkeleton = () => {
   return (
-    <div className="col-span-3 flex h-[41rem] min-h-0 max-w-none animate-pulse flex-col justify-between overflow-hidden text-ellipsis rounded-xl bg-white bg-opacity-40 bg-clip-padding px-0 pb-6 backdrop-blur-lg backdrop-filter lg:col-span-2 xl:col-span-1">
+    <div className="col-span-3 flex h-[41rem] min-h-0 max-w-none animate-pulse flex-col justify-between overflow-hidden text-ellipsis rounded-xl bg-white bg-opacity-40 bg-clip-padding px-0 pb-6 text-gray-300 backdrop-blur-lg backdrop-filter lg:col-span-2 xl:col-span-1">
       <div>
         <div
           className={clsx({
@@ -13,14 +13,14 @@ const ProjectSkeleton = () => {
           <div className="w-full rounded-t-xl bg-gray-200 object-cover" />
         </div>
         <div className="px-4 pt-1">
-          <h2 className="my-3 w-1/3 rounded-lg bg-gray-200 text-xl text-black">
+          <h2 className="my-3 w-1/3 rounded-lg bg-gray-200 text-xl text-gray-200">
             d
           </h2>
           <div className="flex flex-col gap-2">
             {range(4).map((skelText) => (
               <div
                 key={skelText}
-                className="prose-pi:font-light prose rounded-lg bg-gray-200 text-sm !font-light text-black prose-headings:font-extralight prose-p:text-xs prose-li:text-xs prose-li:font-light prose-li:marker:text-black lg:text-xs xl:text-sm"
+                className="prose-pi:font-light prose rounded-lg bg-gray-200 text-sm !font-light text-gray-200 prose-headings:font-extralight prose-p:text-xs prose-li:text-xs prose-li:font-light prose-li:marker:text-black lg:text-xs xl:text-sm"
               >
                 d
               </div>
@@ -34,28 +34,28 @@ const ProjectSkeleton = () => {
       >
         <a
           target="_blank"
-          className="flex cursor-pointer flex-row items-center gap-2 rounded-xl bg-gray-200 p-2 pr-4 text-black"
+          className="flex cursor-pointer flex-row items-center gap-2 rounded-xl bg-gray-200 p-2 pr-4 text-gray-200"
         >
           <div className="flex h-8 w-8 items-center justify-center rounded-full bg-gray-300"></div>
-          <div className="rounded-lg bg-gray-300 text-black">
+          <div className="rounded-lg bg-gray-300 text-gray-300">
             Link to website
           </div>
         </a>
 
         <a
           target="_blank"
-          className="flex cursor-pointer flex-row items-center gap-2 rounded-xl bg-gray-200 p-2 pr-4 text-black"
+          className="flex cursor-pointer flex-row items-center gap-2 rounded-xl bg-gray-200 p-2 pr-4 text-gray-300"
         >
           <div className="flex h-8 w-8 items-center justify-center rounded-full bg-gray-300"></div>
-          <div className="rounded-lg bg-gray-300 text-black">Github</div>
+          <div className="rounded-lg bg-gray-300 text-gray-300">Github</div>
         </a>
 
         <a
           target="_blank"
-          className="flex cursor-pointer flex-row items-center gap-2 rounded-xl bg-gray-200 p-2 pr-4 text-black"
+          className="flex cursor-pointer flex-row items-center gap-2 rounded-xl bg-gray-200 p-2 pr-4 text-gray-300"
         >
           <div className="flex h-8 w-8 items-center justify-center rounded-full bg-gray-300"></div>
-          <div className="rounded-lg bg-gray-300 text-black">2nd Github</div>
+          <div className="rounded-lg bg-gray-300 text-gray-300">2nd Github</div>
         </a>
       </div>
     </div>
--- src/components/Skills/SkillSkeleton.tsx
+++ src/components/Skills/SkillSkeleton.tsx
@@ -2,17 +2,20 @@ import range from "lodash.range";
 
 const SkillSkeleton = () => {
   return (
-    <div className="col-span-3 min-h-full max-w-none animate-pulse overflow-hidden text-ellipsis rounded-xl bg-white bg-opacity-40 bg-clip-padding px-4 pb-6 backdrop-blur-lg backdrop-filter lg:col-span-2 xl:col-span-1">
+    <div className="overflow-hidde col-span-3 min-h-full max-w-none animate-pulse text-ellipsis rounded-xl bg-white bg-opacity-40 bg-clip-padding px-4 pb-6 text-gray-200 backdrop-blur-lg backdrop-filter lg:col-span-2 xl:col-span-1">
       <div className="flex h-20 w-full flex-row items-center justify-between">
-        <h2 className="rounded-xl bg-gray-200 text-lg text-black">
+        <h2 className="rounded-xl bg-gray-200 text-lg text-gray-200">
           dfgdfgdfdfdf! dff{" "}
         </h2>
-        <div className="w-14 rounded-2xl bg-gray-200 text-black">dddd</div>
+        <div className="w-14 rounded-2xl bg-gray-200 text-gray-200">dddd</div>
       </div>
 
-      <div className="prose-pi:font-light prose rounded-xl !font-light prose-headings:font-extralight prose-p:text-xs prose-li:text-xs prose-li:font-light prose-li:marker:text-black">
+      <div className="prose-pi:font-light prose rounded-xl !font-light text-gray-200 prose-headings:font-extralight prose-p:text-xs prose-p:text-gray-200 prose-li:text-xs prose-li:font-light prose-li:marker:text-gray-300">
         {range(5).map((skeletonText) => (
-          <p key={skeletonText} className="rounded-xl bg-gray-200 text-black">
+          <p
+            key={skeletonText}
+            className="rounded-xl bg-gray-200 text-gray-200"
+          >
             Bla bla bla bla bla
           </p>
         ))}
